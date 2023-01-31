2a1c841938aa438bf6e3ae7c0cccc296326da64d
Rishabh Sukhija <79386635+rishabh-4s@users.noreply.github.com>
2023-02-01 01:28:55 +0530

    jhkjhlk

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 // import (useState)
@@ -10,12 +10,20 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum....', author: 'mario', id: 3}
     ]);
     
-    
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blogs => blogs.id!== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    });
 
     return ( 
         <div className="home">
             {/* <h2>Homepage</h2> */}
-            <BlogList blogs={blogs} title="All Blogs"/> 
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> 
             
         </div>
      );
