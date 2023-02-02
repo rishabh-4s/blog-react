811b8609adced79faca9d565ada8984bb408c6cf
Rishabh Sukhija <79386635+rishabh-4s@users.noreply.github.com>
2023-02-03 00:46:31 +0530

    finished project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path='/create' element={<Create/>} />
         <Route path='/blogs/:id' element={<BlogDetails/>} />
+        <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
     </div>
--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
-  return (
-    <div className="create">
-      <h2>Add a New Blog</h2>
-      <form>
-        <label>Blog Title:</label>
-        <input typeof="text" required/>
-        <textarea required></textarea>
-        <label>Blog Author:</label>
-        <select>
-            <option value="mario">mario</option>
-            <option value="yoshi">yoshi</option>
-        </select>
-        <button>Add Blog</button>
-      </form>
-    </div>
+    const [title, setTitle] = useState('e');
+    const [body, setBody] = useState('e');
+    const [author, setAuthor] = useState('yoshi');
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) =>{
+        e.preventDefault(); 
+        const blog = { title, body, author };
+        // console.log(blog);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added');
+            navigate('/');
+        })
+    }
+
+    return (
+        <div className="create">
+        <h2>Add a New Blog</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Blog Title:</label>
+            <input typeof="text" required value={title} onChange={(e)=> setTitle(e.target.value)}/>
+            <textarea required value={body} onChange={(e)=> setBody(e.target.value)}></textarea>
+            <label>Blog Author:</label>
+            <select
+                value={author}
+                onChange={(e)=> setAuthor(e.target.value)}
+            >
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+            </select>
+            <button>Add Blog</button>
+        </form>
+        </div>
   );
 }
  
